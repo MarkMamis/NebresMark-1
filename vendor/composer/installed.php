01d37c375d382840dd75c8fdd2a5f4e015ff3a3c
MarkMamis <nebresmarkedniel22@gmail.com>
2025-09-24 14:53:46 +0800

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-dev-v4',
+        'version' => 'dev-dev-v4',
+        'reference' => 'c0180f55b81548b41cdc0adc7a12c203aab7f4eb',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-dev-v4',
+            'version' => 'dev-dev-v4',
+            'reference' => 'c0180f55b81548b41cdc0adc7a12c203aab7f4eb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
